8ec1c3e08c0409b1007751e51a304ff38396ce6b
nrennie <nrennie35@gmail.com>
2023-05-18 16:14:50 +0100

    fix: save area without extra white space

--- R/save_roughsf.R
+++ R/save_roughsf.R
@@ -5,13 +5,24 @@
 #' @param wait time in seconds to wait for page load
 #' @return No return value, called for side effect
 #' @export
-save_roughsf <- function(rsf,file,background = "white",wait = 4){
-  if(!requireNamespace("pagedown", quietly = TRUE)){
+save_roughsf <- function(rsf, file, background = "white", wait = 4) {
+  if (!requireNamespace("pagedown", quietly = TRUE)) {
     stop("pagedown is needed for this function to work. Please install it.", call. = FALSE)
   }
   tfile <- tempfile(fileext = ".html")
-  format <- substr(file,nchar(file)-2,nchar(file))
-  htmlwidgets::saveWidget(rsf, file = tfile,background = background,selfcontained = TRUE)
-  suppressMessages(pagedown::chrome_print(tfile,output=file,format=format,selector = "canvas#canvas",wait=wait))
+  format <- substr(file, nchar(file) - 2, nchar(file))
+  htmlwidgets::saveWidget(
+    rsf,
+    file = tfile,
+    background = background,
+    selfcontained = TRUE
+  )
+  suppressMessages(pagedown::chrome_print(
+    tfile,
+    output = file,
+    format = format,
+    selector = ".roughsf.html-widget.html-widget-static-bound",
+    wait = wait
+  ))
   suppressMessages(file.remove(tfile))
 }
